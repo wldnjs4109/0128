7d81f6e7d6bbe1dea2932b20273bed6d63d60d7c
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 15:06:10 +0900

    Create 18108.java

---
+++ 18108.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A;
+		Scanner a = new Scanner (System.in);
+		A = a.nextInt();
+		
+		System.out.println(A-543);
+
+	}
+
+}
