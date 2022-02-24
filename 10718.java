b96e84b86ac406a748b5509965eb924c3cb446f1
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 15:03:45 +0900

    Create 10718.java

---
+++ 10718.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
