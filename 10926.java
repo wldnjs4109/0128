6576a6ff0cac933515d801a3db089a0022ee1c3c
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 15:04:39 +0900

    Create 10926.java

---
+++ 10926.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String A;
+		Scanner a = new Scanner (System.in);
+		A = a.nextLine();
+		
+		System.out.println(A+"??!");
+
+	}
+
+}
