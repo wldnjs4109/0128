25b129a50132ba128dcab09281c43ea08449ada8
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 15:01:43 +0900

    Create 1008.java

---
+++ 1008.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double A,B;
+		Scanner a = new Scanner (System.in);
+		A = a.nextDouble();
+		B = a.nextDouble();
+		
+		System.out.println(A/B);
+
+	}
+
+}
