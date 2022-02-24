821ff435c9ab47cfeec4851584dd47692de24336
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 14:59:58 +0900

    Update and rename txt1.txt to 1001.java

---
+++ 1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A,B;
+		Scanner a = new Scanner (System.in);
+		A = a.nextInt();
+		B = a.nextInt();
+		
+		System.out.println(A-B);
+
+	}
+
+}
